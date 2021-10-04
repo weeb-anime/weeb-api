0861e36e62caaf371a366470636c3e49fc9e405b
George Mead <gmeadiv@gmail.com>
2021-10-04 14:40:53 -0700

    adds initial files, dependencies, sets up auth0 and main anime api

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const express = require('express');
+const cors = require('cors');
+const { response } = require('express');
+
+const handlerFunctions = require('./routes.js')
+
+require('dotenv').config()
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(express.json())
+app.use(cors());
+
+app.get('/', (request, response) => {
+  response.send('GEORGE WAS HERE')
+});
+
+app.get('/user', handlerFunctions.getUser);
+
+app.listen(PORT, () => console.log(`listening on Port ${PORT}`))
